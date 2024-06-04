571ecdcdd9eb1e1d24b2938356cd9a6e348daca5
Ilias Elbarnoussi <90471760+IliasElbarnoussi@users.noreply.github.com>
2024-06-04 13:37:00 +0200

    feat: add client with services (#4)
    * feat: implement isHmacValid function to verify HMAC signatures
    
    Signed-off-by: Ilias Elbarnoussi <IliasElbarnoussi@users.noreply.github.com>
    
    * hmac validation with nobles/hashes
    
    * Update package.json
    
    Co-authored-by: Berend Sliedrecht <61358536+berendsliedrecht@users.noreply.github.com>
    
    * add biomejs and pnpm
    
    Signed-off-by: Ilias Elbarnoussi <IliasElbarnoussi@users.noreply.github.com>
    
    * change license in package.json & add license in root
    
    Signed-off-by: Ilias Elbarnoussi <IliasElbarnoussi@users.noreply.github.com>
    
    * add client with the bundled services
    
    Signed-off-by: Ilias Elbarnoussi <IliasElbarnoussi@users.noreply.github.com>
    
    * resolve comments
    
    Signed-off-by: Ilias Elbarnoussi <IliasElbarnoussi@users.noreply.github.com>
    
    * remove generated from gitignore && yarn style fixes
    
    Signed-off-by: Ilias Elbarnoussi <IliasElbarnoussi@users.noreply.github.com>
    
    * gitignore generated directory
    
    Signed-off-by: Ilias Elbarnoussi <IliasElbarnoussi@users.noreply.github.com>
    
    * delete generated directory
    
    Signed-off-by: Ilias Elbarnoussi <IliasElbarnoussi@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Ilias Elbarnoussi <IliasElbarnoussi@users.noreply.github.com>
    Co-authored-by: Ilias Elbarnoussi <IliasElbarnoussi@users.noreply.github.com>
    Co-authored-by: Berend Sliedrecht <61358536+berendsliedrecht@users.noreply.github.com>

---
+++ src/client.ts
@@ -0,0 +1,45 @@
+import {
+  CredentialTemplatesService,
+  OpenAPI,
+  OpenId4VcIssuanceService,
+  OpenId4VcVerificationService,
+  PresentationTemplatesService,
+  ProjectProfileService,
+  ProjectsService,
+  WebhooksService,
+} from '../generated'
+
+export class Client {
+  projects: typeof ProjectsService
+  projectProfile: typeof ProjectProfileService
+  webhooks: typeof WebhooksService
+  templates: {
+    credentials: typeof CredentialTemplatesService
+    presentations: typeof PresentationTemplatesService
+  }
+  openId4Vc: {
+    issuance: typeof OpenId4VcIssuanceService
+    verification: typeof OpenId4VcVerificationService
+  }
+
+  constructor({ apiKey, baseUrl = 'https://api.paradym.id' }: { apiKey: string; baseUrl?: string }) {
+    OpenAPI.HEADERS = {
+      'x-access-token': apiKey,
+    }
+
+    OpenAPI.BASE = baseUrl
+
+    this.projects = ProjectsService
+    this.projectProfile = ProjectProfileService
+    this.webhooks = WebhooksService
+
+    this.templates = {
+      credentials: CredentialTemplatesService,
+      presentations: PresentationTemplatesService,
+    }
+    this.openId4Vc = {
+      issuance: OpenId4VcIssuanceService,
+      verification: OpenId4VcVerificationService,
+    }
+  }
+}
--- src/utils/isHmacValid.ts
+++ src/utils/isHmacValid.ts
@@ -1,9 +1,9 @@
-import { sha256 } from '@noble/hashes/sha256';
-import { hmac } from '@noble/hashes/hmac';
-import { bytesToHex as toHex } from '@noble/hashes/utils';
+import { hmac } from '@noble/hashes/hmac'
+import { sha256 } from '@noble/hashes/sha256'
+import { bytesToHex as toHex } from '@noble/hashes/utils'
 
 export function isHmacValid(secret: string, receivedHmac: string, rawBody: Buffer | string): boolean {
-    const computedHmac = toHex(hmac(sha256, secret, rawBody));
+  const computedHmac = toHex(hmac(sha256, secret, rawBody))
 
-    return computedHmac === receivedHmac;
+  return computedHmac === receivedHmac
 }
