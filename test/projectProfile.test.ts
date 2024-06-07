b03bbf670a60fd1db803d100bab3167687ccd3bf
Ilias Elbarnoussi <90471760+IliasElbarnoussi@users.noreply.github.com>
2024-06-07 15:23:15 +0200

    test generated services (#5)
    * testing the generated code
    
    Signed-off-by: Ilias Elbarnoussi <IliasElbarnoussi@users.noreply.github.com>
    
    * update pnpm lock
    
    Signed-off-by: Ilias Elbarnoussi <IliasElbarnoussi@users.noreply.github.com>
    
    * change server url env
    
    Signed-off-by: Ilias Elbarnoussi <IliasElbarnoussi@users.noreply.github.com>
    
    * openapi-docs url
    
    Signed-off-by: Ilias Elbarnoussi <IliasElbarnoussi@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Ilias Elbarnoussi <IliasElbarnoussi@users.noreply.github.com>
    Co-authored-by: Ilias Elbarnoussi <IliasElbarnoussi@users.noreply.github.com>

---
+++ test/projectProfile.test.ts
@@ -0,0 +1,32 @@
+import { strict as assert } from 'node:assert'
+import { describe, it } from 'node:test'
+import dotenv from 'dotenv'
+import { Client } from '../src/client'
+dotenv.config()
+
+describe('Project Profile', () => {
+  it('should return project profile', async () => {
+    const client = new Client({ apiKey: process.env.X_ACCESS_TOKEN as string })
+    const projectProfile = await client.projectProfile.getProfile({
+      projectId: 'clwt6e610000101s69ubga6lk',
+    })
+
+    assert.ok(projectProfile)
+  })
+
+  it('should update project profile', async () => {
+    const client = new Client({ apiKey: process.env.X_ACCESS_TOKEN as string })
+    const updatedProjectProfile = await client.projectProfile.updateProfile({
+      projectId: 'clwt6e610000101s69ubga6lk',
+      requestBody: {
+        displayName: 'Animo',
+        logo: {
+          altText: 'Logo of Animo Solutions',
+          url: 'https://example.com/logo.png',
+        },
+      },
+    })
+
+    assert.ok(updatedProjectProfile)
+  })
+})
---
+++ test/webhook.test.ts
@@ -0,0 +1,41 @@
+import assert from 'node:assert'
+import { describe, it } from 'node:test'
+import dotenv from 'dotenv'
+import { Client } from '../src/client'
+dotenv.config()
+
+describe('Webhooks', () => {
+  it('should return all webhooks', async () => {
+    const client = new Client({ apiKey: process.env.X_ACCESS_TOKEN as string })
+    const webhooks = await client.webhooks.getWebhooks({
+      projectId: 'clwt6e610000101s69ubga6lk',
+    })
+
+    assert.ok(Array.isArray(webhooks.data))
+    assert.ok(webhooks)
+  })
+
+  it('should create and delete a webhook', async () => {
+    const client = new Client({ apiKey: process.env.X_ACCESS_TOKEN as string })
+    const createdWebhook = await client.webhooks.createWebhook({
+      projectId: 'clwt6e610000101s69ubga6lk',
+      requestBody: {
+        name: 'Test Webhook',
+        url: 'https://example.com/webhook',
+      },
+    })
+
+    assert.ok(createdWebhook)
+
+    await client.webhooks.deleteWebhook({
+      projectId: 'clwt6e610000101s69ubga6lk',
+      webhookId: createdWebhook.id as string,
+    })
+
+    const { data } = await client.webhooks.getWebhooks({
+      projectId: 'clwt6e610000101s69ubga6lk',
+    })
+
+    assert.strictEqual(data.filter((webhook) => webhook.id === createdWebhook.id).length, 0)
+  })
+})
