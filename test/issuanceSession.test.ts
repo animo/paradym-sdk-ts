e17474a6e785bb1ffe63c2edb8e864a011a955ba
Ilias Elbarnoussi <IliasElbarnoussi@users.noreply.github.com>
2024-06-10 14:15:15 +0200

    remove unnecessary createSdjWTcredential method call
    Signed-off-by: Ilias Elbarnoussi <IliasElbarnoussi@users.noreply.github.com>

--- test/issuanceSession.test.ts
+++ test/issuanceSession.test.ts
@@ -11,37 +11,6 @@ describe('Issuance Session', () => {
       projectId: 'clwt6e610000101s69ubga6lk',
     })
 
-    client.templates.credentials.createSdJwtVcTemplate({
-      projectId: 'clwt6e610000101s69ubga6lk',
-      requestBody: { 
-        name: "My SD-JWT VC template", 
-        description: "This is a description", 
-        issuer: "did:web", 
-        background: { 
-          color: "#FFFFFF", 
-          url: "https://example.com/image.png" 
-        }, 
-        text: { 
-          color: "#000000" 
-        }, 
-        validFrom: "2024-04-11", 
-        validUntil: { 
-          start: "validFrom", 
-          future: { years: 5 } 
-        }, 
-        type: "UniversityCard", 
-        revocable: false, 
-        attributes: { 
-          myAttribute: { 
-            type: "string", 
-            name: "My attribute", 
-            description: "This is a attribute", 
-            required: true, 
-            alwaysDisclosed: false 
-          } 
-        } 
-      }
-    })
     assert.ok(issuanceSessions)
     assert.ok(Array.isArray(issuanceSessions.data))
   })
