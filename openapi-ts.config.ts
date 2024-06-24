642bb761a1c766b356dc337d60ae10e32b2dc631
Ilias Elbarnoussi <90471760+IliasElbarnoussi@users.noreply.github.com>
2024-06-24 11:37:42 +0200

    fix type conflicts (#18)
    * fix type conflicts
    
    Signed-off-by: Ilias Elbarnoussi <IliasElbarnoussi@users.noreply.github.com>
    
    * fix styling
    
    Signed-off-by: Ilias Elbarnoussi <IliasElbarnoussi@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Ilias Elbarnoussi <IliasElbarnoussi@users.noreply.github.com>
    Co-authored-by: Ilias Elbarnoussi <IliasElbarnoussi@users.noreply.github.com>

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -16,6 +16,18 @@ const config: UserConfig = {
   output: 'generated',
   services: {
     asClass: true,
+    methodNameBuilder(service, operationId) {
+      const lowerService = service.toLowerCase()
+      const lowerOperationId = operationId.toLowerCase()
+
+      let methodName = operationId
+      if (lowerOperationId.startsWith(lowerService)) {
+        methodName = operationId.slice(service.length)
+      }
+
+      return methodName.charAt(0).toLowerCase() + methodName.slice(1)
+    },
+    operationId: true,
   },
 }
 
--- test/verificationSession.test.ts
+++ test/verificationSession.test.ts
@@ -7,7 +7,7 @@ dotenv.config()
 describe('Verification Session', () => {
   it('should return all issuance sessions', async () => {
     const client = new Paradym({ apiKey: process.env.X_ACCESS_TOKEN as string })
-    const verificationSessions = await client.openId4Vc.verification.getAllOpenId4VcVerificationSessions({
+    const verificationSessions = await client.openId4Vc.verification.getAllVerificationSessions({
       projectId: 'clwt6e610000101s69ubga6lk',
     })
 
