428e4d13747dc3bc70b78d061b3ad70b4d485620
Ilias Elbarnoussi <90471760+IliasElbarnoussi@users.noreply.github.com>
2024-06-24 12:54:28 +0200

    initial major release (#19)
    * small changes for initial major release
    
    Signed-off-by: Ilias Elbarnoussi <IliasElbarnoussi@users.noreply.github.com>
    
    * fix styling
    
    Signed-off-by: Ilias Elbarnoussi <IliasElbarnoussi@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Ilias Elbarnoussi <IliasElbarnoussi@users.noreply.github.com>
    Co-authored-by: Ilias Elbarnoussi <IliasElbarnoussi@users.noreply.github.com>

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -1,16 +1,5 @@
 import type { UserConfig } from '@hey-api/openapi-ts'
 
-// Issues with the configuring through the defineConfig helper function
-// Error(TS4082) |
-// Default export of the module has or is using private name ClientConfig .
-// export default defineConfig({
-//   input: 'https://api.paradym.id/openapi.json',
-//   output: 'src/generated',
-//   services: {
-//     asClass: true,
-//   },
-// })
-
 const config: UserConfig = {
   input: process.env.SERVER ?? 'https://api.paradym.id/openapi-docs.json',
   output: 'generated',
