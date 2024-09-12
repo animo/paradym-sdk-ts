bfe4a7dce2823b7d5a522cfe2345ef776939dfc1
Timo Glastra <timo@animo.id>
2024-09-12 16:03:52 +0200

    chore: update test
    Signed-off-by: Timo Glastra <timo@animo.id>

--- test/projects.test.ts
+++ test/projects.test.ts
@@ -7,7 +7,9 @@ dotenv.config()
 describe('Projects', () => {
   it('should return atleast one project', async () => {
     const client = new Paradym({ apiKey: process.env.X_ACCESS_TOKEN as string })
-    const projects = await client.projects.getAllProjects()
+    const projects = await client.projects.getAllProjects({ 
+      sort: '-createdAt'
+    })
 
     assert.ok(projects.data.length > 0)
   })
