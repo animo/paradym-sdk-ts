025f0763b1ec29e1baf01e87c8051144e685964a
Timo Glastra <timo@animo.id>
2025-10-23 14:28:31 +0200

    fix: do not depend on Buffer
    Signed-off-by: Timo Glastra <timo@animo.id>

--- src/utils/isHmacValid.ts
+++ src/utils/isHmacValid.ts
@@ -3,8 +3,9 @@ import { sha256 } from '@noble/hashes/sha2.js'
 import { bytesToHex as toHex } from '@noble/hashes/utils.js'
 
 export function isHmacValid(secret: string, receivedHmac: string, rawBody: Buffer | string): boolean {
+  const textEncoder = new TextEncoder()
   const computedHmac = toHex(
-    hmac(sha256, Buffer.from(secret), typeof rawBody === 'string' ? Buffer.from(rawBody) : rawBody)
+    hmac(sha256, textEncoder.encode(secret), typeof rawBody === 'string' ? textEncoder.encode(rawBody) : rawBody)
   )
 
   return computedHmac === receivedHmac
